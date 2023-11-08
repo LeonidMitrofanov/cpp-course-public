740475b59177278037b093c6d4802a2972480b8c
Leonid_MacBook <alforfon@gmail.com>
2023-11-08 23:49:18 +0300

    [add] bin_search implementation (task3 complete)

--- tasks/task3/bin_search.hpp
+++ tasks/task3/bin_search.hpp
@@ -1,5 +1,22 @@
 #pragma once
 
+#include <iostream>
+
 int* LowerBound(int* first, int* last, int value) {
-	throw std::runtime_error("not implemented error");
+	int* current;
+	ptrdiff_t length, step;
+	length = std::distance(first,last);
+	while (length > 0){
+		current = first;
+		step = length/2;
+		std::advance(current, step);
+		if (*current < value){
+			first = ++current;
+			length -= step + 1;
+		}
+		else
+			length = step;
+	}
+	return (*first == value? first : last);
+
 }
--- tasks/task3/main.cpp
+++ tasks/task3/main.cpp
@@ -1,11 +1,17 @@
 #include "bin_search.hpp"
 
 #include "iostream"
+#include "stdlib.h"
+#include <vector>
 
 int main()
 {
     // nullptr - не должны падать, вернуть nullptr
     // ТУТ ЗАГЛУШКА, ЧТОБЫ КОД КОМПИЛИРОВАЛСЯ
-    LowerBound(nullptr, nullptr, 0);
+    
+    std::vector<int> v{0,1,2,3,4,5,6,7, 999}; // 999 - значение вне массива
+    std::vector<int> v1;
+
+    std::cout << *LowerBound(v.data(), v.data()+v.size()-1 ,1) << std::endl; 
     return 0;
 }
