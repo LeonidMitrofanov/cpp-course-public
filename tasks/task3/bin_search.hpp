bbdb3a9729e247ad422b69eb2d647ecd02612dfd
Leonid_MacBook <alforfon@gmail.com>
2023-11-09 00:05:05 +0300

    [add] Another bin_search implementation (All test passed)

--- tasks/task3/bin_search.hpp
+++ tasks/task3/bin_search.hpp
@@ -3,20 +3,13 @@
 #include <iostream>
 
 int* LowerBound(int* first, int* last, int value) {
-	int* current;
-	ptrdiff_t length, step;
-	length = std::distance(first,last);
-	while (length > 0){
-		current = first;
-		step = length/2;
-		std::advance(current, step);
-		if (*current < value){
-			first = ++current;
-			length -= step + 1;
+	while (first < last) {
+		int* mid = first + std::distance(first, last) / 2;
+		if (*mid < value) {
+			first = mid + 1;
+		} else {
+			last = mid;
 		}
-		else
-			length = step;
 	}
-	return (*first == value? first : last);
-
+	return first;
 }
